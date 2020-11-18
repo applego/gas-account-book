bdb9108f91e25c2c7249b529fa6202272fd01050
applego <applegorillappa@gmail.com>
2020-11-18 11:24:17 +0900

    add views/Settings.vue

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Settings from '../views/Settings.vue'
 
 Vue.use(VueRouter)
 
@@ -9,6 +10,11 @@ const routes = [
     path: '/',      // パスが "/" のときの設定
     name: 'Home',   // このルートに "Home" という名前をつける
     component: Home // router-view の中に Home コンポーネントを表示する
+  },
+  {
+    path: '/settings',
+    name: 'Settings',
+    component: Settings
   },
   {
     path: '/about',
